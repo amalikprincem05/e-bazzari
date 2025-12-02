b1bb4ba5d7e01bbf65719c8006f6173ef0d9afe1
amalikprincem05 <mayzazmaslam@gmail.com>
2025-12-02 16:30:32 +0500

    Fix Render application css issues

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -26,11 +26,13 @@ Rails.application.configure do
   # Compress CSS using a preprocessor.
   # config.assets.css_compressor = :sass
 
-config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
+
   
   # Do not fall back to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
-  config.assets.digest = true
+config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
+config.assets.digest = true
+config.assets.compile = false
+
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.asset_host = "http://assets.example.com"
